3fbb2a027e78b8327708c39f99f625f20951299b
kongou-ae <y.matsumoto.ae@gmail.com>
2019-02-16 20:46:43 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,6 @@
 Import-Module -Name PowerShellGet -ErrorAction Stop
 Import-Module -Name PackageManagement -ErrorAction Stop
-Register-PsRepository -Default
+Install-PackageProvider -Name NuGet -Force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module -Name AzureRm.BootStrapper
-Use-AzureRmProfile -Profile 2018-03-01-hybrid -Force
-Install-Module -Name AzureStack -RequiredVersion 1.6.0
+Install-Module AzureRM -RequiredVersion 2.4.0
+Install-Module -Name AzureStack -RequiredVersion 1.7.0
